097c27bbe1bd9fa839acf55635e5559420c3408c
Adnan Niaz <sanixstudio@gmail.com>
2023-09-06 00:18:53 -0700

    fixed missing typing issue and removed page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,4 @@
 export { default as Home } from "./Home/Home";
 export { default as AiChatBot } from "./AiChatBot/AiChatBot";
-export { default as Login } from "./Login/Login";
-export { default as Register } from "./Register/Register";
 export { default as Summarizer } from "./Summarizer/Summarizer";
 export { default as NotFound } from "./NotFound/NotFound";
