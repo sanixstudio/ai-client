3a94be7c2b36b830d0f65ad4f9acc09ecee182c4
Adnan Niaz <sanixstudio@gmail.com>
2023-09-06 00:16:36 -0700

    redeploing to vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 import React from "react";
 import "./App.css";
-import { LoginForm, Visitor } from "./components";
+import { Visitor } from "./components";
 
 const App: React.FC = () => {
   return (
     <div className="flex flex-col lg:flex-row w-full h-screen gap-2 overflow-hidden text-xl">
       <Visitor />
-      <span className="hidden text-3xl absolute lg:block top-1/2 left-1/2 -ml-10 -mt-10 rounded-full bg-white p-2.5 py-3 shadow-lg">
-        OR
-      </span>
-      <div className="flex-1 flex justify-center items-center bg-gradient-to-r from-purple-500 to-pink-500 p-2 py-4">
-        <LoginForm />
-      </div>
     </div>
   );
-}
+};
 
-export default App
+export default App;
